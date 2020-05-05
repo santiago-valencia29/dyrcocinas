1f026ea6b2a40285fbf2583f5f4320110bebf1e5
edsvalen <edsvalen@bancolombia.com.co>
2020-05-05 03:52:21 -0500

    ok galeria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './components/pagina-corporativa/inicio/inicio.component';
+import { GaleriaComponent } from './components/pagina-corporativa/galeria/galeria.component';
 
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'pag-not-found' }
+  { path: 'galeria', component: GaleriaComponent },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
 ];
 
 @NgModule({
--- src/app/components/pagina-corporativa/pagina-corporativa.module.ts
+++ src/app/components/pagina-corporativa/pagina-corporativa.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 
 
@@ -9,14 +10,16 @@ import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    InicioComponent
+    InicioComponent,
+    GaleriaComponent
 ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports:[
-    InicioComponent
+    InicioComponent,
+    GaleriaComponent
 
   ]
 })
